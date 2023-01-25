5158281ad0a7fb5b98fd26ffcef087bbf2fc1b75
RememberTo <fdkirill01@yandex.ru>
2023-01-25 16:02:59 +0300

    complete task

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace InterviewTask
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Количество уровней елочки:");
+
+            var levels = int.Parse(Console.ReadLine() ?? string.Empty);
+            var line = new StringBuilder("*");
+            var countEmpty = levels - 1;
+
+            for (var i = 1; i <= levels; i++)
+            {
+                var emptyStr = string.Concat(Enumerable.Repeat(" ", countEmpty--));
+
+                Console.WriteLine(emptyStr + line);
+                line.Append("**");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
